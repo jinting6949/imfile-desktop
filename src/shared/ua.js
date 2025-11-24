ff34b804bbca6ae8f7fe7f3895b8af4eec7006fb
jinting6949 <39321825+jinting6949@users.noreply.github.com>
2025-11-24 11:33:50 +0800

    Update ua.js

--- src/shared/ua.js
+++ src/shared/ua.js
@@ -2,10 +2,16 @@ export const ARIA2_UA = 'aria2/1.37.0'
 export const TRANSMISSION_UA = 'Transmission/3.00'
 export const CHROME_UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'
 export const DU_UA = 'netdisk;6.0.0.12;PC;PC-Windows;10.0.16299;WindowsBaiduYunGuanJia'
+export const DU2_UA = 'LogStatistic'
+export const DU3_UA = 'netdisk;KDown;5.0'
+export const DU4_UA = 'netdisk;P2SP;3.0.20.73'
 
 export default {
   aria2: ARIA2_UA,
   transmission: TRANSMISSION_UA,
   chrome: CHROME_UA,
   du: DU_UA
+  du2: DU2_UA
+  du3: DU3_UA
+  du4: DU4_UA
 }
